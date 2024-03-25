46cacb27e76fcf57b1e2c0605b0ef42c904dcf21
Benedikt Brenk <benedikt.brenk@outlook.de>
2024-03-25 14:09:42 +0100

    fix bugs react-basics

--- react-basics/react-basics_attributes/src/App.js
+++ react-basics/react-basics_attributes/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return Article();
+  return <Article />;
 }
 
 function Article() {
@@ -10,7 +10,7 @@ function Article() {
     <article className="article">
       <h2 className="article__title">Hello World</h2>
       <label htmlFor="input"></label>
-      <input id="input"></input>
+      <input type="text" id="input"></input>
       <a className="article__link" href="https://www.seicentovocale.de">
         Seicento
       </a>
