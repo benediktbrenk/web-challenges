2cb51c09586db0b64567ab92f32ecca0dccce7e1
Benedikt Brenk <benedikt.brenk@outlook.de>
2024-03-28 11:04:00 +0100

    add react-state-2 challenges 1-4

---
+++ react-state-2/react-state-2_personal-details/src/components/Form.js
@@ -0,0 +1,26 @@
+export default function Form({ handleCreateUser }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    const email = event.target.elements.email.value;
+    handleCreateUser(name, email);
+    event.target.reset();
+  }
+
+  return (
+    <form
+      className="form"
+      aria-labelledby="user-details"
+      onSubmit={handleSubmit}
+    >
+      <h2 id="user-details">Please enter your details here!</h2>
+      <label htmlFor="name">Name: </label>
+      <input id="name" name="name" type="text" placeholder="John Doe" />
+      <label htmlFor="email">Email: </label>
+      <input id="email" name="email" type="email" placeholder="john@doe.com" />
+      <button className="form__submit-button" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+}
