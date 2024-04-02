9d344d31e328478265343f9ca245bc0fea518a75
Benedikt Brenk <benedikt.brenk@outlook.de>
2024-04-02 14:36:55 +0200

    add react-effects-and-fetch

---
+++ react-effects-and-fetch/react-effects-and-fetch_api-status/src/components/StatusCheck/index.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./StatusCheck.css";
+
+const apiStatusUrl = "https://example-apis.vercel.app/api/status";
+
+export default function StatusCheck() {
+  const [statusIcon, setStatusIcon] = useState("⁉️");
+
+  async function handleCheckApiStatus() {
+    try {
+      setStatusIcon("⏳");
+      const response = await fetch(apiStatusUrl);
+      setStatusIcon(response.ok ? "✅" : "❌");
+    } catch {
+      setStatusIcon("🚨");
+    }
+  }
+
+  return (
+    <article className="status-check">
+      <div className="status-check__wrapper">
+        <h2 className="status-check__heading">Status:</h2>
+        <span className="status-check__icon">{statusIcon}</span>
+      </div>
+      <button
+        type="button"
+        className="status-check__button"
+        onClick={handleCheckApiStatus}
+      >
+        Check API Status
+      </button>
+    </article>
+  );
+}
